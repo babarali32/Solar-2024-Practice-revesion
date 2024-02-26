7470c13d8c1c55a133b1d1d192b0a28d58002358
Babar Ali <babaraliengineer32@gmail.com>
2024-02-26 16:15:12 +0500

    the feature of planet 01 has been added succefully

--- src/Tester.java
+++ src/Tester.java
@@ -10,7 +10,9 @@ public class Tester {
               solana.heat="100";
               solana.temp="900";
               solana.raduis="200km";
-
+        planet01 obj=new planet01();
+        obj.color="green";
+        obj.size="4343m";
 
 
     }
---
+++ src/planet01.java
@@ -0,0 +1,4 @@
+public class planet01 {
+    String color;
+    String size;
+}
