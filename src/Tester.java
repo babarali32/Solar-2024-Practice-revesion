0c0e04f3158a2e35d61c92cd7c6501c882664148
Babar Ali <babaraliengineer32@gmail.com>
2024-02-26 07:11:27 +0500

    the basic structre is complteted

--- src/Tester.java
+++ src/Tester.java
@@ -2,6 +2,11 @@ public class Tester {
     public static void main(String[] args) {
         solarSystem milkyway=new solarSystem();
 
+        milkyway.sun="1";
+       milkyway.moon="2";
+       milkyway.earth="3";
+
+
     }
 }
 
